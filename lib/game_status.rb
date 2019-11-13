a0e7fe2ab12568e1defeb6e20052b9608a6fe16b
Jason J Cruz <jaycru06@yahoo.com>
2019-11-13 05:26:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,13 +42,7 @@ def over?(board)
 end 
     
 def winner(board) 
-  WIN_COMBINATIONS.each do |combo|
-    if combo == "X" && won?(board) 
-      return "X" 
-    else 
-      return "O"
-    end
-  end
+  
 end
     
     
